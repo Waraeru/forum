82de9e4edc754760afdb54e0851af9ded2162b7b
Your Name <you@example.com>
2018-06-14 15:24:50 +0200

    first commit

---
+++ forum/deconnexion.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+$titre="Déconnexion";
+include("includes/debut.php");
+include("includes/menu.php");
+
+
+//echo '<p>Vous êtes à présent déconnecté <br />
+//Cliquez <a href="'.htmlspecialchars($_SERVER['HTTP_REFERER']).'">ici</a> 
+//pour revenir à la page précédente.<br />
+//Cliquez <a href="./index.php">ici</a> pour revenir à la page principale</p>';
+//echo '</div></body></html>';
+
+header('location:'.$racine.'./../index.php');
+?>
